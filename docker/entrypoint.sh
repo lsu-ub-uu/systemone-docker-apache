f04c7662eada02fdb483a943c93e05f378846437
perebartrolisimo <pere.bartroli@ub.uu.se>
2025-09-03 13:46:17 +0000

    adding dynamic vhost conf

---
+++ docker/entrypoint.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+: "${FITNESSE_CONTEXT_ROOT_ARG:=someContextRoot}"
+
+sed -i "s|{FITNESSE_CONTEXT_ROOT}|${FITNESSE_CONTEXT_ROOT_ARG}|g" /usr/local/apache2/conf/extra/cora-vhosts.conf
+
+supervisord -c /etc/supervisor/conf.d/supervisord.conf
