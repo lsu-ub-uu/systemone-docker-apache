4f5ab6ad874676557e406eb7b88c13558409a4e8
perebartrolisimo <pere.bartroli@ub.uu.se>
2025-09-11 12:13:17 +0000

    add copy of shibboleth2.xml to correct folder

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 : "${FITNESSE_CONTEXT_ROOT_ARG:=someContextRoot}"
 
+#shibboleth2.xml must be in /etc/shibboleth
+cp -f /etc/shibboleth/credentials/shibboleth2.xml /etc/shibboleth/
+
 sed -i "s|{FITNESSE_CONTEXT_ROOT}|${FITNESSE_CONTEXT_ROOT_ARG}|g" /usr/local/apache2/conf/extra/cora-vhosts.conf
 
 supervisord -c /etc/supervisor/conf.d/supervisord.conf
