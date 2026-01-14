7f8b831d8806be66aadb479bd1a990b6b86f810e
perebartrolisimo <pere.bartroli@ub.uu.se>
2026-01-14 12:44:52 +0000

    CORA-3084 Add server name on start of a pod

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -4,6 +4,7 @@
 #shibboleth2.xml must be in /etc/shibboleth
 cp -f /etc/shibboleth/credentials/shibboleth2.xml /etc/shibboleth/
 
+sed -i "s|{SERVER_NAME}|${SERVER_NAME_ARG}|g" /usr/local/apache2/conf/extra/cora-vhosts.conf
 sed -i "s|{FITNESSE_CONTEXT_ROOT}|${FITNESSE_CONTEXT_ROOT_ARG}|g" /usr/local/apache2/conf/extra/cora-vhosts.conf
 
 supervisord -c /etc/supervisor/conf.d/supervisord.conf
